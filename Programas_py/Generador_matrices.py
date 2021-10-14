fbdb3d4560632c79acd234d3a2b944df4305d919
Ineso1 <inesgamos@gmial.com>
2021-10-13 19:46:17 -0500

    Construccion de generadores de matriz

---
+++ Programas_py/Generador_matrices.py
@@ -0,0 +1,56 @@
+import csv
+
+#Variables globales .....................................
+
+registro_progreso = []
+top_global = []
+
+registro_progreso_localidad = "../datos/registro.csv"
+top_global_localidad = "../datos/top_global.csv"
+
+#........................................................
+
+#Genera matriz a partir de un archivo
+#Parametro de ----->>  || archivo.csv inicio || nombre de matriz||
+
+def cargarMatriz(archivo_de_convercion,matriz_generada):
+    archivo_a_convertir = open(archivo_de_convercion)
+    contenido = archivo_a_convertir.readlines()
+    for line in contenido:
+        matriz_generada.append(line.upper().strip().split(','))
+    archivo_a_convertir.close()
+
+#Guarda matriz modificada 
+#parametro de ------>> || archivo.csv destino || matriz modificada 
+def guardarMatriz(archivo_a_guardar,matriz_generada_guardar):
+    string_content = ""
+    for lines in matriz_generada_guardar:
+        for idx, element in enumerate(lines):
+            if idx == len(lines) -1:
+                string_content += str(element) + '\n'
+            else:
+                string_content += str(element) + ','
+
+    string_content = string_content.strip()
+    archivo_productos = open (archivo_a_guardar, 'w')
+    archivo_productos.write(string_content)
+    archivo_productos.close()
+
+
+def matriz_registro_progreso():
+    cargarMatriz(registro_progreso_localidad,registro_progreso)
+    return registro_progreso
+
+def matriz_top_global():
+    cargarMatriz(top_global_localidad,top_global)
+    return top_global
+
+def guardar_registro_progreso(matriz_modificada_registro_progreso):
+    registro_progreso = matriz_modificada_registro_progreso
+    guardarMatriz(registro_progreso_localidad,registro_progreso)
+
+def guardar_top_global(matriz_modificada_top_global):
+    top_global = matriz_modificada_top_global
+    guardarMatriz(top_global_localidad,top_global)
+
+    
---
+++ Programas_py/Principal.py
@@ -0,0 +1,14 @@
+import generador_matrices
+import juego1
+import juego2
+import juego3
+import juego4
+import lib_frames
+import os
+
+def main():
+    pass
+
+
+if __name__=='__main__':
+    main()
